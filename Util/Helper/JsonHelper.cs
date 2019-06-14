7b1117cbcffde8fe0ab2dde75c7ccdd0cb560199
Utor <UtorLin0422@gmail.com>
2019-06-14 20:45:28 +0800

    init project, add base helper, add gitignore

---
+++ Util/Helper/JsonHelper.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace Util.Helper
+{
+    public class JsonHelper
+    {
+
+        public static T Deserialize<T>(string data)
+        {
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        public static string Serialize<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(
+                obj,
+                Formatting.None,
+                new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+        }
+    }
+}
---
+++ Util/Helper/RestSharperHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+
+namespace Util.Helper
+{
+    public class RestSharpHelper
+    {
+        public static T Execute<T>(string baseUrl, string jsonContent)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("application/json", jsonContent, ParameterType.RequestBody);
+            var response = client.Execute(request);
+            return JsonHelper.Deserialize<T>(response.Content);
+        }
+
+        public static async Task<T> ExcutePostAsync<T>(string baseUrl)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest();
+            var restResponse = await client.ExecutePostTaskAsync(request);
+            return JsonHelper.Deserialize<T>(restResponse.Content);
+        }
+
+        public static async Task<T> ExecutePostAsync<T>(string baseUrl, string jsonContent)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest();
+            request.AddParameter("application/json", jsonContent, ParameterType.RequestBody);
+            var postRequest = await client.ExecutePostTaskAsync(request);
+            return JsonHelper.Deserialize<T>(postRequest.Content);
+        }
+
+        public static async Task<T> ExecuteGetAsync<T>(string baseUrl, string parameter)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(Method.GET);
+            var getRequest = await client.ExecuteGetTaskAsync<T>(request);
+            return JsonHelper.Deserialize<T>(getRequest.Content);
+        }
+
+    }
+}
